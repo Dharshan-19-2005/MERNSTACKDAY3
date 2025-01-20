042755fef0b40915ca64a8267d6bf45fb6557911
Dharshan-19-2005 <dharshan.sm2023cse@sece.ac.in>
2025-01-20 09:33:32 +0530

    adding promise.js

---
+++ promise.js
@@ -0,0 +1,20 @@
+ /* function promiseEg(flag)
+{
+    return new Promise(function(resolve,reject){
+    if(flag === 1){
+    resolve("This is  a promise resolve Example")
+    }
+    else if(flag===0){
+        reject("This is promise reject example")
+    }
+    })
+
+}
+promiseEg(1).then((res) =>{
+          console.log(res);
+}).catch((rej)=>{
+    console.log(rej);
+})
+console.log(promiseEg(1)) 
+*/
+
